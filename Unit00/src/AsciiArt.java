8a6b418d536e6aa2153f5de2c36e2c485d4bb777
ericlu28 <eric.andrew.lu@gmail.com>
2020-02-04 13:05:41 -0800

    Added unit 0

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,31 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Eric Lu \n\n" );
+		System.out.println("Bear" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                ^____^                " );
+		System.out.println("               |__  __|         " );
+		System.out.println("               |  <>  |      " );
+		System.out.println("         ^^^  \\______/    ^^^          " );
+		System.out.println("        |   |   |____|    |   |        " );
+		System.out.println("       \\  \\  /_______\\  /  /         " );
+		System.out.println("       \\  \\_/_________\\_/  /            " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(final String[] args)
+	{
+		System.out.println("name \t  date \n\n" );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("BBBBBBBBBBBBBBBBBBBBBBBBB " );
+		System.out.println("CCCCCCCCCCCCCCCCCCCCCCCCC " );
+		System.out.println("DDDDDDDDDDDDDDDDDDDDDDDDD " );
+		System.out.println("EEEEEEEEEEEEEEEEEEEEEEEEE " );
+		System.out.println("FFFFFFFFFFFFFFFFFFFFFFFFF " );
+		System.out.println("GGGGGGGGGGGGGGGGGGGGGGGGG " );
+		System.out.println("HHHHHHHHHHHHHHHHHHHHHHHHH " );
+		System.out.println("IIIIIIIIIIIIIIIIIIIIIIIII " );
+		System.out.println("JJJJJJJJJJJJJJJJJJJJJJJJJ " );
+		System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKK " );
+		System.out.println("LLLLLLLLLLLLLLLLLLLLLLLLL " );
+		System.out.println("MMMMMMMMMMMMMMMMMMMMMMMMM " );
+		System.out.println("NNNNNNNNNNNNNNNNNNNNNNNNN " );
+		System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOO " );
+		System.out.println("PPPPPPPPPPPPPPPPPPPPPPPPP " );
+		System.out.println("QQQQQQQQQQQQQQQQQQQQQQQQQ " );
+		System.out.println("RRRRRRRRRRRRRRRRRRRRRRRRR " );
+
+	}
+}
