8d9c4974725c95849223d492fa5bfa17b103fb54
ericlu28 <eric.andrew.lu@gmail.com>
2020-02-05 08:12:00 -0800

    Unit 1 push

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,45 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+      System.out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+      System.out.print("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+      System.out.print("\n\n");
+   }
+   
+   public void printASmallBox()
+   {	
+      System.out.print("--------------------\n");
+      System.out.print("--------------------\n");
+   }
+ 
+   public void printABigBox()
+   { 	
+      System.out.print("--------------------\n\n\n");
+	   System.out.print("--------------------\n"  );
+   }   
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,62 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes ref = new StarsAndStripes();
+      //call the methods needed to make the patterns on the word document
+      ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printABigBox();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printASmallBox();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printASmallBox();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printASmallBox();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+	   ref.printTwentyStars();
+	   ref.printTwentyDashes();
+   }
+}
